d41a8377dc5cc1ad028067f22a9863054dc7dd65
王木吟 <wmy-zz@163.com>
2021-09-01 19:03:53 +0800

    解决新加好友窗口，Repeater和chatwindow需要大改

--- Client/Repeater.cpp
+++ Client/Repeater.cpp
@@ -63,9 +63,9 @@ void ClientServer::ProcessMsg(Connection::DataType header,const QJsonObject &Dat
 
     case Connection::C2_sychro_history:
     {
-        parseHistoryList(Data);
+
         qDebug()<<"Get history";
-        emit sychro_history();break;
+        emit sychro_history(Data);break;
     }
     case Connection::C4_send_message:
     {
@@ -137,22 +137,6 @@ void ClientServer::processRecievedTextMsg(QJsonObject Data)
 
 }
 
-void ClientServer::parseHistoryList(QJsonObject data)
-{
-    QJsonArray list = data.value("history_list").toArray();
-    for (int i=0; i < list.size(); ++i) {
-        QJsonObject friend_info = list.at(i).toObject();
-        // 下面三行是一条聊天消息
-        ChatRecord Row;
-
-        Row.sender_uid = friend_info.value("sender_uid").toString().toInt();
-        Row.receiver_uid = friend_info.value("receiver_uid").toString().toInt();
-        Row.datetime = friend_info.value("datetime").toString();
-        Row.message = friend_info.value("message").toString();
-
-        HistoryInfo.append(Row);
-    }
-}
 
 void ClientServer::parseFriendList(QJsonObject data)
 {
